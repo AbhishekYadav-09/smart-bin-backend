17fe5cb933245e000d26caa445af2b00fffc32a1
AbhishekYadav-09 <abhishekyadavcode@gmail.com>
2025-09-18 02:01:44 +0530

    chenge in cors

--- src/app.js
+++ src/app.js
@@ -7,8 +7,9 @@ import binRouter from "./routers/bin.routers.js"
 const app = express()
 
 app.use(cors({
-  origin: process.env.CORS_ORIGIN || 'http://localhost:5173',  
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  origin: "https://smartbin-theta.vercel.app",
+  methods: ["GET","POST","PUT","DELETE","OPTIONS"],
+  allowedHeaders: ["Content-Type","Authorization"],
   credentials: true
 }));
 
